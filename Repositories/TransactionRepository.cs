0e9ca20bbc61faeec0baa8a5cbe2d0787945c65d
Klleriston <Klleandrade@outlook.com>
2023-12-26 20:43:22 -0300

    finalizando repositorio de transação

--- Repositories/TransactionRepository.cs
+++ Repositories/TransactionRepository.cs
@@ -1,6 +1,39 @@
 public interface ITransactionRepository
 {
+    void Save(Transaction transaction);
+    void Update(Transaction transaction);
+    Transaction GetById(int transactionId);
+    IEnumerable<Transaction> GetAll();
 }
+
 public class TransactionRepository : ITransactionRepository
 {
+    private readonly DatabaseBank _context;
+
+    public TransactionRepository(DatabaseBank context)
+    {
+        _context = context;
+    }
+
+    public void Save(Transaction transaction)
+    {
+        _context.Transactions.Add(transaction);
+        _context.SaveChanges();
+    }
+
+    public void Update(Transaction transaction)
+    {
+        _context.Transactions.Update(transaction);
+        _context.SaveChanges();
+    }
+
+    public Transaction GetById(int transactionId)
+    {
+        return _context.Transactions.Find(transactionId);
+    }
+
+    public IEnumerable<Transaction> GetAll()
+    {
+        return _context.Transactions.ToList();
+    }
 }
