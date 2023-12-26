7309da2a4f1bd0dff984ca4f82d81e039c97d752
Klleriston <Klleandrade@outlook.com>
2023-12-26 20:43:10 -0300

    finalizando repositorio de transação

---
+++ services/NotificationService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using bank_api;
+
+public class NotificationService
+{
+    private async Task<bool> NotificationOK()
+    {
+        HttpResponseMessage response = await HttpClient("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6");
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public async void SendNotification(User user, string msg)
+    {
+        string email = user.Email;
+        NotificationDTO notificationRequest = new NotificationDTO(email, msg);
+
+        bool notificationOK = await NotificationOK();
+
+        if (!notificationOK)
+        {
+            throw new Exception("Serviço indisponível");
+        }
+    }
+
+    private async Task<HttpResponseMessage> HttpClient(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            return await client.GetAsync(url);
+        }
+    }
+}
--- services/TransactionServices.cs
+++ services/TransactionServices.cs
@@ -1,28 +1,74 @@
+using System.Net;
+using Newtonsoft.Json;
 using bank_api;
 
 public class TransactionServices
 {
-    private UserServices userServices;
+    private readonly UserServices userServices;
     private readonly ITransactionRepository Repository;
     private readonly HttpClient httpClient;
-    public TransactionServices(ITransactionRepository repository)
+
+    public TransactionServices(ITransactionRepository repository, UserServices userServices)
     {
         Repository = repository;
+        this.userServices = userServices;
         httpClient = new HttpClient();
-        
     }
 
-    public void CreateTransaction(TransactionDTO transaction)
+    public async Task<Transaction> CreateTransaction(TransactionDTO transaction)
     {
         User sender = userServices.GetUserById(transaction.senderID);
         User receiver = userServices.GetUserById(transaction.receiverID);
 
         userServices.ValidateTransaction(sender, transaction.value);
+
+        bool isAuthorized = await AuthorizeTransaction(sender, transaction.value);
+
+        if (!isAuthorized)
+        {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction newTransaction = new Transaction
+    {
+        Amount = transaction.value,
+        Sender = sender,
+        Receiver = receiver,
+        timestamp = DateTime.Now
+    };
+
+    sender.UpdateBalance(sender.Balance - transaction.value);
+    receiver.UpdateBalance(receiver.Balance + transaction.value);
+
+    Repository.Update(newTransaction);
+    userServices.AddU(sender);
+    userServices.AddU(receiver);
+
+    return newTransaction;
+        
     }
 
-//CONSTRUIR LOGICA PARA AUTORIZAÇÃO COM MOK
-    // public bool autorizeTransaction(User sender, decimal value)
-    // {
-    //      var mock = "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc";
-    // }
+   private async Task<bool> AuthorizeTransaction(User sender, decimal amount)
+{
+    try
+    {
+        HttpResponseMessage response = await httpClient.GetAsync("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc");
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            dynamic responseJson = JsonConvert.DeserializeObject(content)!;
+            string message = responseJson["message"];
+            return "Autorizado".Equals(message, StringComparison.OrdinalIgnoreCase);
+        }
+        return false;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro na chamada da API de autorização: {ex.Message}");
+        return false;
+    }
+}
+
+
 }
