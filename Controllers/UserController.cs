b152189ca39d220d472f60b92a9093f11103a3d0
Klleriston <Klleandrade@outlook.com>
2023-12-27 17:52:48 -0300

    controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bank_api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly DatabaseBank _context;
+
+    public UserController(DatabaseBank context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] User user)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(new {Mensagem = "Created User!", User = user});
+        }
+        else
+        {
+            return BadRequest(ModelState);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return Ok(users);
+    }
+    
+
+}
---
+++ DTOS/UsersDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace bank_api;
+
+public record class UsersDTO(string firstName, string LastName, string document, string email, string password, decimal balance, Type type)
+{
+
+}
